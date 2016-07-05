653d62a6f084e8d21c6cd8ba26679486f859af79
Robert Zheng <rzheng@xtremeprog.com>
2016-07-05 19:49:37 +0800

    add fsnotify usage

---
+++ example.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+
+	"gopkg.in/fsnotify.v1"
+)
+
+func main() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool, 10)
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				log.Println("event:", event)
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					log.Println("modified file:", event.Name)
+				}
+			case err := <-watcher.Errors:
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add("/home/ax003d")
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
+}
